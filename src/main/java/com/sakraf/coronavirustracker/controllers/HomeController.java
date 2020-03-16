111f064bed272a0a260136f171a23090d2866cb2
sakraf <vlad_farkas@yahoo.com>
2020-03-16 21:32:11 +0200

    initial commit

---
+++ src/main/java/com/sakraf/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.sakraf.coronavirustracker.controllers;
+
+import com.sakraf.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
+        return "home";
+    }
+
+}
